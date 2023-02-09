0e145a2f3aa09cf315d45bfa179c0f7b37dbe34e
Sebastian Rodriguez Ardila <sebasplay2@hotmail.com>
2023-02-08 20:09:19 -0500

    Class 4: UseState

--- curso/src/components/TodoSearch.js
+++ curso/src/components/TodoSearch.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import '../styles/TodoSearch.css';
 
-const onSearch = (e) =>{
-  console.log(e.target.value)
-}
-
 function TodoSearch() {
+
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const onSearch = (e) =>{
+    console.log(e.target.value);
+    setSearchValue(e.target.value);
+  }
+
   return (
     <input 
     className="TodoSearch" placeholder="Perro"
+    value={searchValue}
     onChange = {onSearch} />
   );
 }
